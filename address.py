0f7b8ae6954d53ebf4a378f64817375ee18f4efe
Russell Bigley <wx8ball@gmail.com>
2017-05-17 22:46:20 -0600

    feat: Add address audit function

---
+++ address.py
@@ -0,0 +1,47 @@
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
+            "Trail", "Parkway", "Commons"]
+
+#mapping the abbreviations of the street names
+mapping = { "St": "Street",
+            "St.": "Street",
+            "Rd." : "Road",
+            "Ave" : "Avenue",
+            "Cir" : "Circle",
+            "Ct"  : "Court"
+            }
+
+k_types = set()
+
+def audit_k_attributes(elem):
+    return elem.attrib['k']
+
+def audit_street_type(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
+
+def is_street_name(elem):
+    return (elem.attrib['k'] == "addr:street")
+
+#had to relook at the regular expression I was using
+#It was matching all long names and replacing
+#creating erronious names such as AveAvenue
+def update_name(name, mapping):
+    st = re.compile('St\.?\s?$')
+    name = st.sub("Street",name)
+    rd = re.compile('Rd\.?\s?$')
+    name = rd.sub("Road", name)
+    ave = re.compile('Ave\.?\s?$')
+    name = ave.sub("Avenue", name)
+    dr = re.compile('Dr.?\s?$')
+    name = dr.sub('Drive',name)
+    cir = re.compile('Cir\.?\s?$')
+    name = cir.sub('Circle',name)
+    ct = re.compile('Ct\.?\s?$')
+    name = ct.sub('Court',name)
+
+    return name
