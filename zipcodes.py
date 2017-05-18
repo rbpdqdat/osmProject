dca9bccec6bc605bab5780975c6c518eb0f31ee9
Russell Bigley <wx8ball@gmail.com>
2017-05-17 22:45:19 -0600

    feat: add zipcode audit function

---
+++ zipcodes.py
@@ -0,0 +1,24 @@
+def is_zip(elem):
+    return (elem.attrib['k'] == "addr:postcode")
+
+def audit_zip_codes(zip_code):
+    #making zip codes consistent
+    #filter out possible non-numeric zip codes
+    #I didn't find any non-numeric zip codes, but I 
+    #thought it was likely
+    nonnumber = re.compile('[A-Za-z]')
+    if nonnumber.search(zip_code) is not None:
+        zip_code = '99999'
+    if len(zip_code)<5:
+        zip_code = '99999'
+    #follow conventional length of 5 digits
+    if len(zip_code)>6 :
+        #unsupported operand type| zip_beginning.match(zip_code)==False :
+        #split zipcode if 'dash' present
+        # or take the first 5 digits
+        if (re.search('-',zip_code) is not None):
+            zip_code=zip_code.split('-')[0]
+            zip_code = zip_code[0:5]
+        else:
+            zip_code = zip_code[0:5]    
+    return zip_code
