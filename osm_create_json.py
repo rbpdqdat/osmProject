eabdf6045e8b41d2e54e469b3a6c1454160079a7
Russell Bigley <wx8ball@gmail.com>
2017-05-18 10:36:35 -0600

    refactor: Major rewrite to clean up json creation
    The old method was breaking the data up too much and making the
    mongo query only work on node or way (unless a more complex query
    structure was created).  It was deemed easier to rewrite the code
    that writes the json file.

--- osm_create_json.py
+++ osm_create_json.py
@@ -12,51 +12,64 @@ import address
 
 OSM_PATH = "denverMetro.osm"
 
+k_types = set()
 LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
 PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 # Make sure the fields order in the csvs matches the column order in the sql table schema
-FIELDS = ['type','pos','id', 'lat', 'lon', 'user', 'uid', 'version', 'changeset', 'timestamp']
+CREATE_META = ['version', 'uid', 'user', 'changeset', 'timestamp']
 
 street_types = defaultdict(set)
 
-def shape_element(element, attr_fields=FIELDS, 
-                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
-
-    nodeway_attribs = {}
-    tags = []  # Handle secondary tags the same way for both node and way elements
-
+def shape_element(element, meta_fields=CREATE_META, 
+                default_tag_type='regular'):
+    nodeway = {}
+    #simple check to determine if the parent element is 
+    #of type 'node' or tyep 'way'  
     if element.tag == 'node' | element.tag == 'way':
-        for attrib in element.attrib:
-            #if attrib in FIELDS:
-            #    nodeway_attribs[attrib] = element.attrib[attrib]
+        node['type'] = element.tag
+        node['pos'] = [floatorNone(element.get("lat")), floatorNone(element.get("lon"))]
+        node['id'] = element.get("id")
+        node['visible'] = element.get("visible")
+        node["created"] = {}
+        for key in meta_fields:
+            nodeway["created"][key] = element.get(key)
+        for child in element.getchildren:
+            parseChild(node,child):
+         return node
+    else:
+        return None
 
-        addressDict = {}
-        for child in element:
-            node_tag = {}
-            if is_zip(child):
-                    child.attrib['v'] = audit_zip_codes(child.attrib['v'])
-            if LOWER_COLON.match(child.attrib['k']):
-                if (child.attrib['k'].split(':',1)[0] == 'addr'):
-                    if (child.attrib['k'].split(":")[1] == 'postcode'):
-                        addressDict.update({'zipcode':child.attrib['v']})
-                    else:
-                        addressDict.update({child.attrib['k'].split(':',1)[1]:child.attrib['v']})
-            elif PROBLEMCHARS.match(child.attrib['k']):
-                continue
-            elif (child.attrib['k'] == 'phone'):
-                node_tag['phone'] = child.attrib['v']
-                nodeway_attribs.update(node_tag)
-            else:
-                node_tag['type'] = 'regular'
-                node_tag['key'] = child.attrib['k']
-                node_tag['value'] = child.attrib['v']
-                nodeway_attribs.update(node_tag)
+#parsing the child elements of the 'way' and 'node' types
+def parseChild(node,children):
+    k = children.get("k")
+    r = children.get("r")
+    #create a unique list of the key types
+    k_types.add(k)
+    if k and not re.search(problemchars, k):
+    #if is_zip(child):
+    #    child.attrib['v'] = audit_zip_codes(child.attrib['v'])
+        if LOWER_COLON.match(k):
+            if (k.split(':',1)[0] == 'addr'):
+                node["address"] = node["address"] if node.get("address") else {}
+                if (k.split(":")[1] == 'postcode'):
+                    #audit the zip code to a standard format
+                    node["address"]["zipcode"] = audit_zip_codes(children.attrib['v'])
+                elif is_street_name(k):
+                    #audit the street names to a standard format
+                    node["address"]["street"] = audit_street_type(street_types, 
+                             children.attrib['v'])(k.split(":")[1] == 'street')
+               else:
+                    node["address"][k.split(':',1)[1]] = children.attrib['v']
+        elif ( k == 'phone'):
+            #audit phone numbers to a standard format
+            node['phone'] = audit_phone_number(children.attrib['v'])
+        else:
+            node[k] = children.attrib['v']
     
-            if any(addressDict.keys()) == True :
-                node_tag['address'] = addressDict
-                nodeway_attribs.update(node_tag)
-        return {'node': nodeway_attribs}
+    elif r:
+        node["node_refs"] = node["node_refs"] if node.get("node_refs") else []
+        node["node_refs"].append(ref)
         
 
 # ================================================== #
