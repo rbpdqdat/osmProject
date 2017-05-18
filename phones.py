664537ad1d228bd9d3431f83b42170aa153749df
Russell Bigley <wx8ball@gmail.com>
2017-05-17 22:45:52 -0600

    feat: add phone number auditing function

---
+++ phones.py
@@ -0,0 +1,28 @@
+#convert alphabet phone characters to actual phone numbers
+#
+def phone_letter_tonum(alphaphone):
+    char_numbers = [('abc',2), ('def',3), ('ghi',4), ('jkl',5), ('mno',6), ('pqrs',7), ('tuv',8), ('wxyz',9)]
+    char_num_map = {c:v for k,v in char_numbers for c in k}
+    return("".join(str(char_num_map.get(v,v)) for v in alphaphone.lower()))
+
+def is_phone(elem):
+    return (elem.attrib['k'] == "phone")
+
+def audit_phone_number(phone):
+    #found some columns had 2 phone numbers
+    phone = phone.split(";")[0]
+    phone = re.sub("^\++0+1?","+1",phone)
+    #some phone numbers were purposely words
+    # such as '1-888-AMC-4FUN' for business 
+    #the phone number needed to be converted to properly work
+    if re.search("[A-Za-z]",phone):
+        phone = phone_letter_tonum(phone)
+    if (re.search('^[\+1]',phone) == None):
+        phone = missphone
+    #additional substitution to make sure the 
+    #country code was in the phone number
+    phone = re.sub("^1+","+1",phone)
+    z = phonenumbers.parse(phone, "US")
+    phone = phonenumbers.format_number(z, phonenumbers.PhoneNumberFormat.NATIONAL)
+
+    return phone
